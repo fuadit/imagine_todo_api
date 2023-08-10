85ba33f189249462d851f62b1df99328959120a2
Fuad Mehawish <fuad.sale7@gmail.com>
2023-08-10 20:53:09 +0300

    Add Sanctum Package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('user_type')->default(2); // 1 Admin , 2 User
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2023_08_10_174055_create_tasks_table.php
+++ database/migrations/2023_08_10_174055_create_tasks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('due_date')->nullable();
+            $table->integer('status')->default(1);
+            $table->foreignId('assignee')->nullable();
             $table->timestamps();
         });
     }
